c2044a917c00eabaa227c86c0c0d9d16c301fe7f
Masaki Waga <masakiwaga@gmail.com>
2018-06-26 19:32:59 +0900

    added submodule of cruise control model

--- experiments/BrCCGen.m
+++ experiments/BrCCGen.m
@@ -5,15 +5,6 @@
 
 clear;
 InitBreach;
-fuel_inj_tol = 1.0; 
-MAF_sensor_tol = 1.0;
-AF_sensor_tol = 1.0; 
-pump_tol = 1.;
-kappa_tol=1; 
-tau_ww_tol=1;
-fault_time=50;
-kp = 0.04;
-ki = 0.14;
 
 warning('off', 'Simulink:LoadSave:EncodingMismatch')
 mdl = 'cruise_ctrl_kpit';
