51a7b837ff523cf8a9e0787f421fa419a26c2cd1
Masaki Waga <masakiwaga@gmail.com>
2018-07-03 10:35:10 +0900

    the experiment script works on linux, too

--- experiments/run_gtime.sh
+++ experiments/run_gtime.sh
@@ -6,13 +6,19 @@ readonly date_str=$(date +%m%d%H%M%S)
 readonly DATA_DIR=data/10.0
 mkdir -p log/$date_str
 
+if [[ $(uname) == Darwin ]]; then
+    GTIME=/usr/local/bin/gtime
+else
+    GTIME=/usr/bin/time
+fi
+
 cd $(dirname $0)
 
-cd ../build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j3 filt && cd -
+cd ../build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j3 qtpm && cd -
 
 ## Settling
 for input in $DATA_DIR/*.tsv; do
     for t in $(seq $RUN_TIMES); do
-        /usr/local/bin/gtime -v -o log/$date_str/qtpm-${input##*/}-$t.log ../build/qtpm -i $input -f ./saturation.dot --abs > /dev/null
+        $GTIME -v -o log/$date_str/qtpm-${input##*/}-$t.log ../build/qtpm -i $input -f ./saturation.dot --abs > /dev/null
     done
 done
