b65eb0cb9c9fe00041cac8db97e117588c35c599
Masaki Waga <masakiwaga@gmail.com>
2018-07-04 18:30:10 +0900

    added experiment script for different sampling frequencies

---
+++ experiments/run_gtime_density.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+readonly RUN_TIMES=20
+
+readonly date_str=$(date +%m%d%H%M%S)
+readonly DATA_DIR=data/density
+mkdir -p log/$date_str
+
+if [[ $(uname) == Darwin ]]; then
+    GTIME=/usr/local/bin/gtime
+else
+    GTIME=/usr/bin/time
+fi
+
+cd $(dirname $0)
+
+cd ../build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j3 qtpm && cd -
+
+## Settling
+for input in $DATA_DIR/*.tsv; do
+    for t in $(seq $RUN_TIMES); do
+        $GTIME -v -o log/$date_str/qtpm-${input##*/}-$t.log ../build/qtpm -i $input -f ./saturation.dot --abs > /dev/null
+    done
+done
