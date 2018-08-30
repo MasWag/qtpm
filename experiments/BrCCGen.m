d2272180e968d4614750724f90b11a754bba8e2c
Masaki Waga <masakiwaga@gmail.com>
2018-08-30 11:23:41 +0900

    Added inline documentation in the monitoring input generators

--- experiments/BrCCGen.m
+++ experiments/BrCCGen.m
@@ -1,8 +1,24 @@
-% scipt created to generate Monitoring input
-% 
-% Masaki Waga
-% Created: 2018/06/26
+%% Scipt to Generate Monitoring Input of Cruise Control Model for
+%% Different Simulation Time
+% This script generates monitoring inputs (tsv files) for the experiment of our
+% implementation of quantitative timed pattern matching using the cruise
+% control model <https://github.com/ERATOMMSD/cruise-control-simulink>
+%
+% * Author: Masaki Waga
+% * Created: 2018/06/26
+%
 
+%% The Output Format
+%
+% * $t$: the abosolute time
+% * $v$: the velocity of the car
+% * $v_{\mathit{ref}}$: the reference velocity of the car
+%
+% The output format is as follows.
+%
+% $$t, v, v_{\mathit{ref}}, |v - v_{\mathit{ref}}|$$
+
+%% Initialize Breach and load the Simulink model
 clear;
 InitBreach;
 
@@ -10,31 +26,42 @@ warning('off', 'Simulink:LoadSave:EncodingMismatch')
 mdl = 'cruise_ctrl_kpit';
 BrCC = BreachSimulinkSystem(mdl);
 
-speed_ref_gen = pulse_signal_gen({'SpeedRef'}); % Generate a pulse signal for speed ref
+%% Define the input generator for the reference speed
+% Generate a pulse signal for speed ref
+speed_ref_gen = pulse_signal_gen({'SpeedRef'}); 
 
 InputGen = BreachSignalGen({speed_ref_gen});
 
+%%
+% The input to the model is completely periodic and without any random
+% noise.
 InputGen.SetParam({'SpeedRef_base_value', 'SpeedRef_pulse_period', ...
                    'SpeedRef_pulse_amp','SpeedRef_pulse_width'}, ... 
                   [20 500 30 .5]);
 
 BrCC.SetInputGen(InputGen);
 
+%% Run the model changing the simulation time
+% The simulation time and the number of the sampled values is
+% changed but the sampling frequency is fixed (100 Hz?).
+
 for t =  1:10
     simTime = 60 * 1000 * t;
     
     tmp = BrCC.copy;
+    %%
+    % It samples the data every 10 unit time (ms ?) and finish the
+    % execution after 60 * 1000 * t unit time
     tmp.Sim(0:10.0:simTime);
-    figure; 
-    tmp.PlotSignals({'velocity', 'ref_speed'});
+    %figure; 
+    %tmp.PlotSignals({'velocity', 'ref_speed'});
 
+    %%
+    % It writes the log to a tsv file.
     dlmwrite(sprintf('BrCCPulse-%d.tsv', simTime), ...
              vertcat(tmp.P.traj{1,1}.time, tmp.P.traj{1,1}.X(6,:), ...
                      tmp.P.traj{1,1}.X(3,:), abs(tmp.P.traj{1,1}.X(6,:) ...
                                                   - tmp.P.traj{1,1}.X(3,:)))','delimiter', '\t','precision', 10);
 end
-% BrCC.P.traj{1,1}.X(1:2,:)
-% vertcat(BrCC.P.traj{1,1}.time, BrCC.P.traj{1,1}.X(1:2,:))
-% [BrCC.P.traj{1,1}.time', BrCC.P.traj{1,1}.X(1:2,:)']'
 
 return
