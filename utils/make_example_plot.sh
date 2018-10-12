4ccd8a3a81bd9344c9f85f66af14b6c4ac1ab001
Masaki Waga <masakiwaga@gmail.com>
2018-10-12 20:04:04 +0900

    modified the plot in the paper

--- utils/make_example_plot.sh
+++ utils/make_example_plot.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)
 
-../build/qtpm -i ../example/signal.txt -f ../example/small.dot | ./toGnuplot.sh | cat output_pdf.plt - | gnuplot 
+../build/qtpm -i ../example/paper.txt -f ../example/paper.dot | ./toGnuplot.sh | cat output_pdf.plt - | gnuplot 
 pdfcrop example_qtpm.pdf
 mv example_qtpm-crop.pdf example_qtpm.pdf
