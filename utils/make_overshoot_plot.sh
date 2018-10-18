e5554bbdd6ba41ddc6914d33030a196f06e4d8b0
Masaki Waga <masakiwaga@gmail.com>
2018-10-18 13:03:33 +0900

    updated the heatmap

--- utils/make_overshoot_plot.sh
+++ utils/make_overshoot_plot.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)
 
-head -n 70 ../experiments/data/10.0/BrCCPulse-60000.tsv | tail -n 30 | ../build/qtpm -af ../experiments/overshoot.dot | ./toGnuplot.sh 450:700 | cat ./output_pdf.plt <(echo 'set output "example_overshoot.pdf"') - | gnuplot
+head -n 70 ../experiments/data/10.0/BrCCPulse-60000.tsv | tail -n 30 | ../build/qtpm -af ../experiments/overshoot.dot | ./toGnuplot.sh 450:700 | cat ./output_pdf_experiment.plt <(echo 'set output "example_overshoot.pdf"'; echo 'set xtics 450,100'; echo 'set ytics 450,100'; echo "set cbtics ('-2' -2, '-6' -6, '-10' -10, '-14' -14, '-18' -18);") - | gnuplot
 pdfcrop example_overshoot.pdf
 mv example_overshoot-crop.pdf example_overshoot.pdf
---
+++ utils/output_pdf_experiment.plt
@@ -0,0 +1,4 @@
+set terminal pdf font ",40"
+set xtics offset 0, 0.5
+set ytics offset 0.5, 0
+
